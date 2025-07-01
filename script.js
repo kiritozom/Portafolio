a672ec8e5b72ebd7339c9c96396cb67d16b9476e
kiritozom <nicolas.btd03@gmail.com>
2025-07-01 15:31:55 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-document.getElementById("honkai").onclick
-
-function otrapagina()
+function OtraPagina()
 {
+    window.location.href="pagina1.html"
+}
 
+function VolverPagina()
+{
+    window.location.href="index.html"
 }
